1ec09d8a85d6dc8bc11ebe592b98397415e366a8
Nikhil Gupta <100pranjalgupta@gmail.com>
2025-08-14 23:35:01 +0530

    Update db.config.js

--- backend/DB/db.config.js
+++ backend/DB/db.config.js
@@ -1,5 +1,7 @@
 const { PrismaClient } = require('../generated/prisma');
 
+const isDev = process.env.NODE_ENV === 'development';
+
 exports.prisma = new PrismaClient({
-    log: ['query']
+    log: isDev ? ['query', 'info', 'warn', 'error'] : ['error']
 });
